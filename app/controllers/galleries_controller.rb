44fd6ec741230576e1b5ed3040d0ed7f54db0b11
omar gallo <omargallob@gmail.com>
2010-08-10 23:38:42 +0100

    gallery update fix

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,9 @@
+class GalleriesController < ApplicationController
+  def index
+  end
+
+  def show
+    @gallery = Gallery.find_by_title(params[:title].gsub(/-/," "))
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ActionController::Routing::Routes.draw do |map|
        admin.root :controller => "overview"
     end
 
- 
+  map.gallery_show '/Photo-Gallery/:title', :controller=> "galleries", :action => "show"
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
